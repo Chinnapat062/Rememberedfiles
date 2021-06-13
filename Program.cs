b704d4ab0de71cbaa4bd9b107d30147694f7aefa
Chinnapat062 <78645438+Chinnapat062@users.noreply.github.com>
2021-06-13 17:14:09 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ระบบจำไฟล์
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] file = new string[5];
+            Console.WriteLine("Please input number from 0 - 3 to choose the following menu");
+            Console.WriteLine("[0] Show all recent files");
+            Console.WriteLine("[1] Open new file");
+            Console.WriteLine("[2] Open recent file");
+            Console.WriteLine("[3] Exit");
+
+            int choosemenu = int.Parse(Console.ReadLine());
+            if(choosemenu == 0)
+            {
+                Console.Write(file);
+                Main();
+            }
+            if (choosemenu == 1)
+            {
+                string needfile = Console.ReadLine();
+                Main();
+            }
+            if (choosemenu == 2)
+            {
+                int sequncefile = int.Parse(Console.ReadLine());
+            }
+            return;
+        }
+        
+    }
+}
